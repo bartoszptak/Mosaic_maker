159184a19fc126d0b176604cce2693a75a23757c
Bartosz <bartptak@gmail.com>
2019-09-18 18:39:43 +0200

    Add upload files

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
 import mosaic as ms
-
+import os
 
 app = Flask(__name__)
 
@@ -12,9 +13,16 @@ def index():
                            app_title=APP_TITLE,
                            tiles=ms.get_mosaic_list())
 
-@app.route('/makebeautiful', methods=['POST'])
+@app.route('/makebeautiful', methods=['GET', 'POST'])
 def makebeautiful():
-    pass
+    if request.method == 'POST':
+        file = request.files['file']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join('data','images', filename))
+
+    return render_template('index.html', 
+                           app_title=APP_TITLE,
+                           tiles=ms.get_mosaic_list())
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
