c2d5ef68aefccf01ace949264f130608cf7b54fc
Bartosz Ptak <bartptak@gmail.com>
2019-09-19 15:38:29 +0200

    Performance mosiac maker

--- app/main.py
+++ app/main.py
@@ -1,16 +1,17 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, send_file, after_this_request
 from werkzeug.utils import secure_filename
 import mosaic as ms
 import os
 
 app = Flask(__name__)
 
-APP_TITLE = 'Mosaic maker'
+app.config['DOWNLOAD_FOLDER'] = os.path.join('data','images')
+app.config['TITLE'] = 'Mosaic maker'
 
 @app.route("/")
 def index():
    return render_template('index.html', 
-                           app_title=APP_TITLE,
+                           app_title=app.config['TITLE'],
                            tiles=ms.get_mosaic_list())
 
 @app.route('/makebeautiful', methods=['GET', 'POST'])
@@ -18,11 +19,20 @@ def makebeautiful():
     if request.method == 'POST':
         file = request.files['file']
         filename = secure_filename(file.filename)
-        file.save(os.path.join('data','images', filename))
+        file.save(os.path.join(app.config['DOWNLOAD_FOLDER'], filename))
 
-    return render_template('index.html', 
-                           app_title=APP_TITLE,
-                           tiles=ms.get_mosaic_list())
+
+    return redirect('/')
+
+@app.route('/getfile/<filename>')
+def getfile(filename):
+    
+    @after_this_request
+    def cleanup(response):
+        os.remove(os.path.join(app.config['DOWNLOAD_FOLDER'], filename))
+        return response
+
+    return send_file(app.config['DOWNLOAD_FOLDER'], filename, as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=False,host='0.0.0.0')
--- app/mosaic.py
+++ app/mosaic.py
@@ -1,5 +1,17 @@
 import cv2
 import numpy as np
+import numpy.ma as ma
+import time
+
+COLORS = 32
+FILE_PATH = 'data/website.jpg'
+TILE = 'circle'
+SCALE = 25
+
+### TO DELETE ###
+start = time.time()
+#################
+
 
 def get_mosaic_list():
     from glob import glob
@@ -9,61 +21,53 @@ def get_mosaic_list():
         result.append(file.split('/')[-1].split('.')[0].title())
     return sorted(result)
 
-# COLORS = 32
-# FILE_PATH = 'data/website.jpg'
-# TILE = 'beer'
-# SCALE = 25
 
-# # Load image
-# org = cv2.imread(FILE_PATH)
-# tile = cv2.imread(f'data/tiles/{TILE}.png')
+# Load image
+org = cv2.imread(FILE_PATH)
+tile = cv2.imread(f'data/tiles/{TILE}.png')
 
-# # Resize image
-# img = cv2.resize(org, (0,0), fx=1/(SCALE/2), fy=1/(SCALE/2))
-# tile = cv2.resize(tile, (SCALE, SCALE))
-# tile = tile.astype('float')
+# Resize image
+img = cv2.resize(org, (0, 0), fx=1/(SCALE/2), fy=1/(SCALE/2))
+tile = cv2.resize(tile, (SCALE, SCALE))
+tile = tile.astype('float')
 
+# Make less colors
 
-# # Make less colors
-# def quantize_colors(img, colors):
-#     img = img.astype('float')
-#     img = np.round(img/255*colors)/colors*255
-#     return img.astype('uint8')
+def quantize_colors(img, colors):
+    img = img.astype('float')
+    return np.round(img/255*colors)/colors*255
 
-# img = quantize_colors(img, COLORS)
-
-# # Output image
-# result = np.ones((img.shape[0]*SCALE, img.shape[1]*SCALE, 3), dtype=np.float32)*255
-
-# def make_move_color(result, tile, scale, yx, bgr):
-#     patt = tile.copy()
-#     for y in range(patt.shape[0]):
-#         for x in range(patt.shape[1]):
-#             if (patt[y][x] != np.array([255,255,255])).all():
-#                 patt[y][x] += bgr
-#                 if (patt[y][x] > np.array([255,255,255])).any():
-#                     b,g,r = patt[y][x]
-#                     if b > 255:
-#                         patt[y][x][0]=patt[y][x-1][0]
-#                     if g > 255:
-#                         patt[y][x][1]=patt[y][x-1][1]
-#                     if r > 255:
-#                         patt[y][x][2]=patt[y][x-1][2]
+img = quantize_colors(img, COLORS)
 
 
-#     y, x = yx[0]*SCALE, yx[1]*SCALE
-#     result[y:y+SCALE, x:x+SCALE] = patt
+# Output image
+result = np.ones((img.shape[0]*SCALE, img.shape[1]
+                  * SCALE, 3), dtype=np.float32)*255
 
-# # Move colors
-# for y in range(img.shape[0]):
-#     for x in range(img.shape[1]):
-#         make_move_color(result, tile, SCALE, (y,x), img[y,x])
 
-# result = result.astype('uint8')
-# result = cv2.resize(result, (org.shape[1], org.shape[0]))
+def make_move_color(result, tile, scale, yx, bgr):
+    patt = tile.copy()
+    patt += bgr
+    patt[ma.masked_where(patt > 255, patt).mask] = 255.
 
-# cv2.imshow('image', org)
-# cv2.waitKey(0)
-# cv2.imshow('image', result)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
+    y, x = yx[0]*SCALE, yx[1]*SCALE
+    result[y:y+SCALE, x:x+SCALE] = patt
+
+
+# Move colors
+for y in range(img.shape[0]):
+    for x in range(img.shape[1]):
+        make_move_color(result, tile, SCALE, (y, x), img[y, x])
+
+result = result.astype('uint8')
+result = cv2.resize(result, (org.shape[1], org.shape[0]))
+
+### TO DELETE ###
+print(f'{time.time()-start} s')
+
+cv2.imshow('image', org)
+cv2.waitKey(0)
+cv2.imshow('image', result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+#################
