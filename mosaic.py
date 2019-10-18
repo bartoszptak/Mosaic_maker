d9eabb86013c429e11df5434466fab2873ab1bb4
Bartosz Ptak <bartptak@gmail.com>
2019-10-18 09:44:16 +0200

    Add docstring

--- mosaic.py
+++ mosaic.py
@@ -6,6 +6,7 @@ import time
 COLORS = 32
 
 def get_mosaic_list():
+    """ Return the list of mosaic titles """
     from glob import glob
     files = glob('data/tiles/*.png')
     result = []
@@ -14,21 +15,25 @@ def get_mosaic_list():
     return sorted(result)
 
 def read_all(file, tile):
+    """ Return the loaded image and tile """
     org = cv2.imread(file)
     tile = cv2.imread(f'data/tiles/{tile}.png')
     return org, tile
 
 def resize_all(img, tile, size):
+    """ Resize image elements """
     img = cv2.resize(img, (0, 0), fx=1/(size/2), fy=1/(size/2))
     tile = cv2.resize(tile, (size, size))
     tile = tile.astype('float')
     return img, tile
 
 def quantize_colors(img, colors):
+    """ Reduce the number of colors in the image """
     img = img.astype('float')
     return np.round(img/255*colors)/colors*255
 
 def make_move_color(result, tile, scale, yx, bgr):
+    """ Move a tile of the right color to the image """
     patt = tile.copy()
     patt += bgr
     patt[ma.masked_where(patt > 255, patt).mask] = 255.
@@ -37,6 +42,7 @@ def make_move_color(result, tile, scale, yx, bgr):
     result[y:y+scale, x:x+scale] = patt
 
 def get_mosaic(img, tile, size, shape):
+    """ A function that creates a mosaic """
     result = np.ones((img.shape[0]*size, img.shape[1]
                     * size, 3), dtype=np.float32)*255
 
@@ -48,6 +54,7 @@ def get_mosaic(img, tile, size, shape):
     return cv2.resize(result, (shape[1], shape[0]))
 
 def make_mosaic(img_file, tile_file, size):
+    """ The main function that loads the image, processes and saves """
     img, tile = read_all(img_file, tile_file)
     shape = img.shape
     img, tile = resize_all(img, tile, size)
